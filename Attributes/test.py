3794a9f34eab1c98457c8fb35407b3fcd6e8bbc3
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-20 13:45:41 +0900

    [Initial] - Doing on GPU Computer

---
+++ Attributes/test.py
@@ -0,0 +1,9 @@
+import face_alignment
+from skimage import io
+
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+
+input = io.imread('../test/assets/aflw-test.jpg')
+preds = fa.get_landmarks(input)
+# import torch
+# print(torch.__version__)
